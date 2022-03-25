e3f65883c1c4aca4c40f451bb19f4ee412aa9d3f
GytAndr <andriejunas@gmail.com>
2022-03-25 14:34:30 +0300

    first working version

---
+++ script.js
@@ -0,0 +1,43 @@
+document.getElementById("submit").addEventListener("click", addBookToLibrary);
+
+//library array that will be looped and displayed on screen.
+let myLibrary = [];
+
+//Book constructor
+function Book(title, author, pages, read) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
+}
+// function to reset input fields
+function resetInput() {
+	title.value = "";
+	author.value = "";
+	pages.value = "";
+	read.value = "";
+}
+
+//function pushes newly created Book obj into array.
+function addBookToLibrary() {
+	//get input values
+	let title = document.getElementById("title").value;
+	let author = document.getElementById("author").value;
+	let pages = document.getElementById("pages").value;
+	let read = document.getElementById("read").value;
+	//put into new object using construnctor
+	let thing = new Book(title, author, pages, read);
+	//put into array
+	myLibrary.push(thing);
+	//reset input fields
+	resetInput();
+	printArray();
+}
+function printArray() {
+	let list = document.getElementById("list");
+	let p = document.createElement("p");
+	for (let index = 0; index < myLibrary.length; index++) {
+		p.innerText = `${myLibrary[index].title} by ${myLibrary[index].author}, ${myLibrary[index].pages} pages, ${myLibrary[index].read}`;
+		list.appendChild(p);
+	}
+}
